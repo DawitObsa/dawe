37fda2cceb426e17661aa957bfe716a88f219c44
Abenezer <133504684+Abenezerzewdu@users.noreply.github.com>
2025-01-24 18:15:28 +0000

    Update LabAssistant.java

--- DailyProject/src/main/java/com/mycompany/dailyproject/LabAssistant.java
+++ DailyProject/src/main/java/com/mycompany/dailyproject/LabAssistant.java
@@ -11,5 +11,8 @@ public LabAssistant(String name, String id, String assignedLab) {
         System.out.println("Lab Assistant " + name + " is assisting in the " + assignedLab + " lab.");
     }
 
-    
+    // views which assistant is assigned to which lab
+     public void viewAssignedLab() {
+        System.out.println("Lab Assistant " + name + " is assigned to the " + assignedLab + " lab.");
+    }
 }
